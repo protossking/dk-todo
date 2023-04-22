23aa54d9a3ecf7d85081997fbe4a3529f1b153fa
bear.yang(양석조)/dkt <bear.yang@kakaocorp.com>
2023-04-22 19:29:31 +0900

    init

---
+++ src/main/java/com/dk/todo/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.dk.todo.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        String[] paths = {"/api/**"};
+
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        Info info = new Info().title("APO ADMIN BE").version("v1.0");
+
+
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
+                .info(info);
+
+    }
+}
