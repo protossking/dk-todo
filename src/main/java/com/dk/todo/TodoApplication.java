eb40971f5b9000cb1138b38a830d1c64607ed0d7
protossking <upgrademarine5@gmail.com>
2023-06-16 19:42:26 +0900

    add: Set TimeZone Asia/Seoul

--- src/main/java/com/dk/todo/TodoApplication.java
+++ src/main/java/com/dk/todo/TodoApplication.java
@@ -3,8 +3,16 @@ package com.dk.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TodoApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
