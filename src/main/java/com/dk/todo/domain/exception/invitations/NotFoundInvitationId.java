b9e70665aaedfc20a53a38d8a181b13a4a81a319
protossking <upgrademarine5@gmail.com>
2023-09-04 09:42:17 +0900

    fix: MESSAGE 상수로 변경

--- src/main/java/com/dk/todo/domain/exception/invitations/NotFoundInvitationId.java
+++ src/main/java/com/dk/todo/domain/exception/invitations/NotFoundInvitationId.java
@@ -2,7 +2,9 @@ package com.dk.todo.domain.exception.invitations;
 
 public class NotFoundInvitationId extends RuntimeException {
 
+    private static final String MESSAGE = "찾을 수 없는 Invitation Id 입니다.";
+
     public NotFoundInvitationId() {
-        super("찾을 수 없는 Invitation Id 입니다.");
+        super(MESSAGE);
     }
 }
