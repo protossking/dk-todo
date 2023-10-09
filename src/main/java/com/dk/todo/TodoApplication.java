9641645fd0f6872cd09f88d8ffd5d144563df50b
protossking <upgrademarine5@gmail.com>
2023-10-09 15:56:15 +0900

    fix: Swagger

--- src/main/java/com/dk/todo/TodoApplication.java
+++ src/main/java/com/dk/todo/TodoApplication.java
@@ -1,5 +1,7 @@
 package com.dk.todo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://gosimtodo-dev.kro.kr")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class TodoApplication {
